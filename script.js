3a4e27c1ea28770fcb4545d1fc2ec77e8aea3f05
unknown <abeltran9@mail.valenciacollege.edu>
2023-10-06 10:36:50 -0400

    Added text file to repo

--- script.js
+++ script.js
@@ -6,30 +6,20 @@ var randomNumber = Math.floor(Math.round(Math.random()))
 randomNumber = Math.round(Math.random());
 if (randomNumber === 0) {
     computerChoice = "heads";
-} else  computerChoice = "tails"
+} else computerChoice = "tails"
 
-if(coinflip === computerChoice){
+if (coinflip === computerChoice) {
 alert ("You guessed right! The coin flip landed on " + computerChoice)
-    } else 
-    alert ("Sorry, the coin flip landed on " + computerChoice);
-
-     if (userChoice === computerChoice){
-    alert("You guessed right! The coin flip landed on heads"); 
-    }
-  if (randomNumber === 1) {
-        computerChoice = "tails";
-    if (userChoice === computerChoice){
-        alert("You guessed right! The coin flip landed on tails");
-
-
-    } 
-    else {alert("You guessed right! The coin flip landed on heads");}
-    
+    } else {
+    alert ("Sorry, the coin flip landed on " + computerChoice);   
 } 
 
-let = prompt("What year were you born?");
-const birthYear = 1995;
-
-if ((2023-birthYear) > 21) {
-    alert("You are old enough to drink in the US");
-} 
+const birthYear = prompt("What year you were born?");
+const age = 2023 - birthYear
+if (age > 21) {
+    alert ("You are old enough to drink in the US");
+} else if (age == 21) {
+    alert ("You are old enough to drink in the US... barely");
+} else {
+    alert ("Sorry, you are not old enough to drink in the US");
+}
